7d0a8e97ebefb590783929b4d42193cd80f9606d
liu-ameng <liuche911@gmail.com>
2014-07-31 11:27:44 +0800

    Initial Commit

---
+++ MyWorkRecord/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyWorkRecord
+//
+//  Created by 刘澈 on 14-7-31.
+//  Copyright (c) 2014年 刘澈. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
